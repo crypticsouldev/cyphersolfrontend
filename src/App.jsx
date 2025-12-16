1369348775aa3ea82b5b5a5794f62e5837fecdef
crypticsouldev <kalamadnan5@gmail.com>
2025-12-16 07:43:22 +0000

    start frontend with vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+         frontend commit
+      </div>
+      
+    </>
+  )
+}
+
+export default App
